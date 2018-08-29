40668ea1e740e684f6a173f7c052f9dd7e08ae26
johnstonjacob <johnstonajacob@gmail.com>
2018-08-29 02:46:37 +0000

    add image removal to cleanup function

--- main.go
+++ main.go
@@ -20,15 +20,46 @@ import (
 )
 
 func main() {
-	code := `package main
+	code := `
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	for i := 0; i < 100; i++ {
-		fmt.Println("hi", i)
+	// Seeding with the same value results in the same random sequence each run.
+	// For different numbers, seed with a different value, such as
+	// time.Now().UnixNano(), which yields a constantly-changing number.
+	rand.Seed(time.Now().UnixNano())
+	answers := []string{
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes definitely",
+		"You may rely on it",
+		"As I see it yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",
+		"My reply is no",
+		"My sources say no",
+		"Outlook not so good",
+		"Very doubtful",
 	}
-}`
+	fmt.Println("Magic 8-Ball says:", answers[rand.Intn(len(answers))])
+}
+
+	`
 	New(code)
 }
 
@@ -117,6 +148,11 @@ func cleanup(cli *client.Client, id string) error {
 
 	err := cli.ContainerRemove(ctx, id, config)
 
+	if err != nil {
+		return err
+	}
+
+	_, err = cli.ImageRemove(ctx, "gotest", types.ImageRemoveOptions{Force: true})
 	if err != nil {
 		return err
 	}
